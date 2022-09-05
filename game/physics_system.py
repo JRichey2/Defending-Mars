46ba911551a16d2e81e587dcfc5391b81baf9ee8
Steven Wilson <stevenwilson500@gmail.com>
2022-09-05 17:57:51 -0400

    Adjusting thrust power and turn speed

--- game/physics_system.py
+++ game/physics_system.py
@@ -20,14 +20,14 @@ class PhysicsSystem(ecs.System):
             if inputs.w:
                 acceleration += V2.from_degrees_and_length(rotation + 90, 1.0)
             if inputs.a:
-                acceleration += V2.from_degrees_and_length(rotation + 180, 1.0)
+                acceleration += V2.from_degrees_and_length(rotation + 180, 0.2)
             if inputs.s:
-                acceleration += V2.from_degrees_and_length(rotation + 270, 1.0)
+                acceleration += V2.from_degrees_and_length(rotation + 270, 0.2)
             if inputs.d:
-                acceleration += V2.from_degrees_and_length(rotation, 1.0)
+                acceleration += V2.from_degrees_and_length(rotation, 0.2)
 
             if acceleration.length > 0:
-                acceleration.normalize()
+                #acceleration.normalize()
                 acceleration *= 0.3
 
 
@@ -42,8 +42,8 @@ class PhysicsSystem(ecs.System):
             window.camera_position.x = physics.position.x
             window.camera_position.y = physics.position.y
 
-            physics.rotation -= 2.0 if inputs.e else 0
-            physics.rotation += 2.0 if inputs.q else 0
+            physics.rotation -= 2.0 * time_factor if inputs.e else 0
+            physics.rotation += 2.0 * time_factor if inputs.q else 0
 
             emitter = entity['emitter']
             if emitter is None:
